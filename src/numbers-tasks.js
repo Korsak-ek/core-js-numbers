8cba24eb804e0d4790916c5182ea9a3526bb0fd8
Korsak.Kate <korsak.ek@gmail.com>
2023-12-13 21:22:08 +0300

    feat: solve tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -383,8 +383,8 @@ function toPrecision(number, precision) {
  * new Number(5) => 5
  * Number(-5)    => -5
  */
-function getNumberValue(/* number */) {
-  throw new Error('Not implemented');
+function getNumberValue(number) {
+  return number.valueOf();
 }
 
 /**
@@ -402,8 +402,11 @@ function getNumberValue(/* number */) {
  * 5        => true
  * '5'      => false
  */
-function isNumber(/* number */) {
-  throw new Error('Not implemented');
+function isNumber(number) {
+  if (number.valueOf() === 'number') {
+    return true;
+  }
+  return false;
 }
 
 /**
